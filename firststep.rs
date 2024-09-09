8cf2ebdb97a97fbb7ec6a86dbc7867c6c44722bb
Alexander-Fransson <festusOp7@gmail.com>
2024-09-09 20:50:37 +0200

    [feat] made the first rust file

---
+++ firststep.rs
@@ -0,0 +1,8 @@
+// runs automatically when the program is started
+
+fn main() {
+    println!("Hello, world!");
+}
+
+// to compile this rust program run `rustc firststep.rs`
+// to run this rust program run `./firststep`
